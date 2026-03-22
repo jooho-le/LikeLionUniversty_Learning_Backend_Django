c9cb239be767ed3fa00f5183088fb5088ebc8e6a
jooho-le <wngh0606@naver.com>
2026-03-22 18:55:36 +0900

    리스트 정의 및 선언

---
+++ Python_FirstStep/list.py
@@ -0,0 +1,17 @@
+# 리스트 (list)
+
+mbti = ['INTJ', 'INTP', 'INFJ', 'INFP']
+
+print(mbti)
+print(mbti[0])
+
+mbti[0] = 'ENFP' # 리스트에 새로운 값 할당 
+print(mbti)
+print(mbti[0])
+# -> 첫번째 요소 변경이 됨 
+
+my_list = [123, 'apple'] # 파이썬은 데이터가 이렇게 일관성 없어도 가능 
+print(my_list)
+# 이렇게 하면 다른 for문이나 할때 문제가 될 수 있음
+# 리스트에 넣을 타입 고려는 해야함 
+
